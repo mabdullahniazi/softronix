5742da7f6247d40d70d023db746ebfa798825d3d
abdullahniaziuni <nabdullah482@gmail.com>
2026-02-14 08:34:07 +0500

    chore: Delete an unspecified file.

--- mamo/template/client/src/components/UserAnalytics.tsx
+++
@@ -1,6 +0,0 @@
-import UserAnalyticsComponent from "../pages/Admin/UserAnalytics";
-
-// This is a wrapper component to reuse the UserAnalytics component in the Admin Dashboard
-export default function UserAnalytics() {
-  return <UserAnalyticsComponent />;
-}
--- mamo/template/server/pt1/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
-
-// Get dashboard statistics (admin only)
-router.get('/stats', isAuthenticated, isAdmin, dashboardController.getDashboardStats);
-
-module.exports = router;
